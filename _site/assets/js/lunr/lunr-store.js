50726ef20e6b737714486b6bc8b957bfb7e02984
Hyunsoo Lim <hyunsoolim@Hyunsooui-MacBook-Pro.local>
2021-08-16 18:11:24 +0900

    setting 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "One Hot Encoding",
         "excerpt":"Multi Classification 변이 유전자 탐색 및 분류 머신러닝과 딥러닝 이론을 어느정도 공부하고 진짜 문제와 이론을 접목시켜보기 위해 캐글 같은 프로젝트에 도전해보고 있다. 현재는 캐글에서 변이 유전자 탐색 프로젝트에 도전하고 있는데, 결국 1~9까지의 Mulit classification 유형이다. 문제는 feature가 Gene, Variation(변이), Text로 전부 categorical이라는 것. Text feature의 경우 길이도 엄청 길어서, NPL에...","categories": ["classification"],
         "tags": ["Blog, classification, categorical, 범주형, NLP"],
-        "url": "http://localhost:4000/blog/classification/One-Hot-Encoding/",
+        "url": "/blog/classification/One-Hot-Encoding/",
         "teaser": null
       },{
         "title": "Response Coding",
         "excerpt":"범주형 데이터 Response Coding One-hot-encoding에 이어서 Response Coding은 조건부 확률을 일컫는 것이나 다름없다.참조 수식은 다음과 같은데 \\[P(class=X | category=A) = P(category=A ∩ class=X) / P(category=A)\\] category에 따른 class의 확률을 구하는 것이라, Category의 개수만큼 차원(feature)이 생기는 원핫인코딩과는 달리 Response Coding은 class의 개수만큼 차원(feature)이 늘어난다. 또한 클래스당 확률을 고려하는 거라 모델의 측정...","categories": ["classification"],
         "tags": ["Blog"],
-        "url": "http://localhost:4000/blog/classification/responce-coding/",
+        "url": "/blog/classification/responce-coding/",
         "teaser": null
       }]
